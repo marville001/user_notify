a2a0d6ebbd51cca38180714168cebf2962348b51
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-28 09:46:40 +0300

    Update template and dockerfile

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 "use strict";
-const assert = require("assert");
+// const assert = require("assert");
 
-const { EMAIL_FROM, EMAIL_PASS } = process.env;
+// const { EMAIL_FROM, EMAIL_PASS } = process.env;
 
-assert(EMAIL_FROM, "EMAIL_FROM is required");
-assert(EMAIL_PASS, "EMAIL_PASS is required");
+// assert(EMAIL_FROM, "EMAIL_FROM is required");
+// assert(EMAIL_PASS, "EMAIL_PASS is required");
 
-module.exports = {
-    email: EMAIL_FROM,
-    email_pass: EMAIL_PASS,
-};
+// module.exports = {
+//     email: EMAIL_FROM,
+//     email_pass: EMAIL_PASS,
+// };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
@@ -11,9 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 8000;
+// For testing templates
+app.set("views", path.join(__dirname, "templates"));
+app.set("view engine", "ejs");
+
 
 // API Docs endpoint
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.get("/ejs/proj", (req, res) => {
+  res.render("confirmEmail", {
+    name: "Alex",
+    link: "https://user.com/register",
+    description: "web app to",
+  });
+});
+
+const PORT = process.env.PORT || 9006;
 app.listen(PORT, console.log(`App running on port ${PORT}`));
